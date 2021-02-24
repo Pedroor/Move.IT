cb648c845f80e38b42e858d002da2cef098a04ce
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-02-24 09:22:49 -0300

    Day 2- Config Next project and create Profile,CountDown and CompletedChallenges Components

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,19 +1,15 @@
-import '../styles/global.css'
+import styles from '../styles/components/ExperienceBar.module.css'
 
-export function ExperienceBar(){
-    return(
-        <header className="experience-bar"> 
+export function ExperienceBar() {
+    return (
+        <header className={styles.experienceBar}>
             <span>0 xp</span>
-
             <div>
-                <div style={{ width:'60%'}}/>
+                <div style={{ width: '50%' }} />
 
-                <span className="current-experience" style={{ left:'50%'}}>
-                    300xp
-                </span>
+                <span className={styles.currentExperience} style={{ left: '50%' }}>300 xp</span>
             </div>
-           
-            <span>600xp</span>
+            <span>600 xp</span>
         </header>
-    )
+    );
 }
